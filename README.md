b2594ce45cb5ba20b2aafe5c4d2a95295a337551
Dominic Bunch <dbunch@google.com>
2019-07-05 10:00:33 +0200

    Added datastore emulator support in store unit tests.

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 [![Travis Build Status](https://api.travis-ci.org/google/emitto.svg?branch=master)](https://travis-ci.org/google/emitto)
 [![Go Report Card](https://goreportcard.com/badge/github.com/google/emitto)](https://goreportcard.com/report/github.com/google/emitto)
 
-### About
+## About
 
 Emitto is a service which provides a robust and targeted way to manage, store,
 and administer [Suricata](https://suricata-ids.org/) intrusion detection system
 (IDS) rules to a distributed network sensor monitoring deployment.
 
-### Building
+## Building
 
 1) Install [Bazel](https://bazel.build/)
 
@@ -22,12 +22,12 @@ cd emitto/
 bazel build //...
 ```
 
-#### Prerequisites
+### Prerequisites
 
 The following services and products must be established and configured before
 running Emitto.
 
-##### Fleetspeak
+#### Fleetspeak
 
 Emitto leverages [Fleetspeak](https://github.com/google/fleetspeak) for
 reliable, multi-homed communication between the server and network sensors.
@@ -41,18 +41,18 @@ information and updates.
 Please read the Fleetspeak
 [disclaimer](https://github.com/google/fleetspeak#disclaimer).
 
-##### Google Cloud Platform
+#### Google Cloud Platform
 
 By default, Emitto uses [Google Cloud Datastore](https://cloud.google.com/datastore/)
 and [Google Cloud Storage](https://cloud.google.com/storage/) for object and rule file
 storage, respectively.
 
-### Discussions & Announcements
+## Discussions & Announcements
 
 The [Emitto](https://groups.google.com/forum/#!forum/emitto) Google Groups
 forum
 will be used for community discussions and announcements.
 
-### DISCLAIMER
+## DISCLAIMER
 
 This is not an officially supported Google product.
---
+++ source/server/store/datastore_test.go
@@ -0,0 +1,66 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package store
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"cloud.google.com/go/datastore"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc"
+)
+
+// This test relies on the Google Cloud Datastore emulator with the following configuration
+// constants:
+const (
+	testHost    = "localhost:9999"
+	testProject = "test-project-name"
+)
+
+func TestDataStore(t *testing.T) {
+	RunTestSuite(t, func() (Store, error) {
+		// Reset emulator before each test.
+		if err := resetEmulator(); err != nil {
+			return nil, err
+		}
+		c, err := datastore.NewClient(context.Background(), testProject, option.WithEndpoint(testHost), option.WithoutAuthentication(), option.WithGRPCDialOption(grpc.WithInsecure()))
+		if err != nil {
+			return nil, fmt.Errorf("GCD client creation failed: %v", err)
+		}
+		return &DataStore{c}, nil
+	})
+}
+
+func resetEmulator() error {
+	resp, err := http.Post(fmt.Sprintf("http://%s/reset", testHost), "", bytes.NewBuffer([]byte{}))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if string(body) != "Resetting...\n" {
+		return errors.New("emulator failed to reset")
+	}
+	return nil
+}
